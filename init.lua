b1e56713b3145d8e0c17761f061775f76fbd86b1
Thiago Araujo da Silva <thiagos.araujo09@gmail.com>
2025-10-29 02:19:48 -0300

    add telescope version

--- init.lua
+++ init.lua
@@ -256,6 +256,7 @@ require('lazy').setup({
 
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
     event = 'VimEnter',
     dependencies = {
       'nvim-lua/plenary.nvim',
